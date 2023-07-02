765d9c1d5a8cde9f62303bd00d9394cc18cfe53d
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-03 00:03:38 +0500

    Image schema / Builder

--- src/app/siteComponents/SwipeCard.tsx
+++ src/app/siteComponents/SwipeCard.tsx
@@ -1,11 +1,14 @@
+"use Client";
 import React from "react";
 import { client } from "@/lib/sanityClient";
-import { Image } from "sanity";
+import { Image as IImage } from "sanity";
+import Image from "next/image";
+import { urlForImage } from "../../../sanity/lib/image";
 
 export const getProducts = async () => {
   const res = await client.fetch(`*[_type=="product" ]{
     title,
-    image,
+    image[], 
     price,
    }`);
   return res;
@@ -13,14 +16,27 @@ export const getProducts = async () => {
 
 interface IProduct {
   title: string;
-  image: Image;
+  image: IImage[];
   price: number;
 }
 
 const SwipeCard = async () => {
   const data: IProduct[] = await getProducts();
 
-  return <div>SwipeCard</div>;
+  return (
+    <div className="">
+      {data.slice(0, 3).map((item) => (
+        <div className="">
+          <Image
+            src={urlForImage(item.image[0]).url()}
+            alt={item.title}
+            height={250}
+            width={250}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SwipeCard;
