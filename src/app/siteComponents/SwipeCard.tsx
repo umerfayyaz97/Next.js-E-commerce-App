6d2a68e7753959be0852e4ba7e9c53d2b9598fbd
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-05 01:07:46 +0500

    hover animation

--- src/app/siteComponents/SwipeCard.tsx
+++ src/app/siteComponents/SwipeCard.tsx
@@ -25,27 +25,53 @@ const SwipeCard = async () => {
 
   return (
     <>
-      <div className="text-center ">
-        <h3 className="text-xs font-semibold text-blue-600 ">PRODUCTS</h3>
-        <h1 className="text-2xl font-bold ">Check What We Have</h1>
+      <div className="text-center">
+        <h3 className="text-xs font-semibold text-blue-600">PRODUCTS</h3>
+        <h1 className="text-2xl font-bold">Check What We Have</h1>
         <br></br>
       </div>
-      <div className="grid grid-cols-[repeat(3,auto)] justify-center gap-x-6">
+      <div className="grid grid-cols gap-y-2 md:grid-cols-[repeat(3,auto)] justify-center  gap-x-6">
         {data.slice(0, 3).map((item) => (
-          <div className="">
-            <Image
+          <div className="transition-all duration-300 transform product-card hover:scale-105">
+            <img
               src={urlForImage(item.image[0]).url()}
               alt={item.title}
               height={300}
               width={300}
+              // className="w-full h-auto"
             />
-            <h2 className="">{item.title}</h2>
-            <h3 className="">{item.price}</h3>
+            <h2 className="font-bold text-center">{item.title}</h2>
+            <h3 className="font-bold text-center">${item.price}</h3>
           </div>
         ))}
       </div>
     </>
   );
 };
+
+// ------------------------------------------------------------
+//     <>
+//       <div className="text-center ">
+//         <h3 className="text-xs font-semibold text-blue-600 ">PRODUCTS</h3>
+//         <h1 className="text-2xl font-bold ">Check What We Have</h1>
+//         <br></br>
+//       </div>
+//       <div className="grid grid-col  md:grid-cols-[repeat(3,auto)] justify-center gap-x-6">
+//         {data.slice(0, 3).map((item) => (
+//           <div className="">
+//             <Image
+//               src={urlForImage(item.image[0]).url()}
+//               alt={item.title}
+//               height={300}
+//               width={300}
+//             />
+//             <h2 className="font-bold text-center">{item.title}</h2>
+//             <h3 className="font-bold text-center">${item.price}</h3>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
 
 export default SwipeCard;
