6c4481b112d992e4252eec48e18e097c05720647
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-05 22:08:54 +0500

    Swipe cards addition and different from other

---
+++ src/app/siteComponents/DifferentFromOthers.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const DifferentFromOthers = () => {
+  return (
+    <>
+      <div className="grid justify-end h-full grid-cols-4">
+        <div className="items-end col-start-4 row-span-2 text-4xl font-bold tracking-tight break-normal">
+          Unique and Authentic Vintage Designer Jewellery
+        </div>
+      </div>
+      <br></br>
+      <br></br>
+    </>
+  );
+};
+
+export default DifferentFromOthers;
