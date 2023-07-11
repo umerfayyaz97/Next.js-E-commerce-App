6b26454df676fc79e90499a62ff28b20fd568d8d
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-11 23:42:43 +0500

    slight changes

--- src/app/Details/[id]/page.tsx
+++ src/app/Details/[id]/page.tsx
@@ -35,66 +35,6 @@ const ProductDescription = async ({ params }: { params: { id: string } }) => {
   const details = data.find((i) => i._id === params.id)?.details;
   const care = data.find((i) => i._id === params.id)?.care;
 
-  //   return (
-  //     <>
-  //       {image && (
-  //         <div className="flex">
-  //           <div className="w-1/4">
-  //             <Image
-  //               src={urlForImage(image[0]).url()}
-  //               alt="image"
-  //               height={300}
-  //               width={300}
-  //             />{" "}
-  //             {/* Display small image */}
-  //           </div>
-  //           <div className="w-1/2">
-  //             <Image
-  //               src={urlForImage(image[0]).url()}
-  //               alt="image"
-  //               height={300}
-  //               width={300}
-  //             />{" "}
-  //             {/* Display enlarged image */}
-  //           </div>
-  //           <div className="w-1/4">
-  //             <h1 className="text-2xl font-bold">{title}</h1>
-  //             <p className="text-lg">Title</p>
-  //             <p className="text-lg">Available Sizes:</p>
-  //             <div className="flex items-center mt-4">
-  //               <button className="px-4 py-2 mr-2 bg-gray-300">-</button>
-  //               <input
-  //                 className="w-16 px-4 py-2 text-center"
-  //                 type="number"
-  //                 defaultValue="1"
-  //               />
-  //               <button className="px-4 py-2 ml-2 bg-gray-300">+</button>
-  //             </div>
-  //             <button className="px-4 py-2 mt-4 text-white bg-blue-500">
-  //               Add to Cart
-  //             </button>
-  //           </div>
-  //           <div>
-  //             <h2 className="mt-8 text-2xl font-bold">Price</h2>
-  //             <hr className="my-4" />
-  //             <p>{price}</p>
-  //           </div>
-  //           <div>
-  //             <h2 className="mt-8 text-2xl font-bold">Product Details</h2>
-  //             <hr className="my-4" />
-  //             <p>{details}</p>
-  //           </div>
-  //           <div>
-  //             <h2 className="mt-8 text-2xl font-bold">Product Care</h2>
-  //             <hr className="my-4" />
-  //             <p>{care}</p>
-  //           </div>
-  //         </div>
-  //       )}
-  //     </>
-  //   );
-  // };
-
   return (
     <>
       {image && (
