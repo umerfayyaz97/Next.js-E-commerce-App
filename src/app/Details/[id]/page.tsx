45e5d2dfef47cd018c57b2625a1a48c4ae0d367a
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-19 17:15:23 +0500

    Slight changes

--- src/app/Details/[id]/page.tsx
+++ src/app/Details/[id]/page.tsx
@@ -39,49 +39,61 @@ const ProductDescription = async ({ params }: { params: { id: string } }) => {
     <>
       {image && (
         <div className="flex flex-col">
-          <div className="flex">
-            <div className="w-1/4">
-              <Image
-                src={urlForImage(image[0]).url()}
-                alt="image"
-                height={100}
-                width={100}
-              />{" "}
-              {/* Display small image */}
-            </div>
-            <div className="w-1/2">
-              <Image
-                src={urlForImage(image[0]).url()}
-                alt="image"
-                height={300}
-                width={300}
-              />{" "}
-              {/* Display enlarged image */}
-            </div>
-            <div className="flex flex-col justify-between w-1/4">
-              <div>
-                <h1 className="text-2xl font-bold">{title}</h1>
-                <div className="flex items-center">
-                  <p className="text-lg">Title</p>
-                  <h2 className="mt-8 ml-4 text-2xl font-bold">{price}</h2>
+          <div className="flex flex-wrap">
+            <div className="w-full md:w-1/2">
+              <div className="w-full md:hidden">
+                <Image
+                  src={urlForImage(image[0]).url()}
+                  alt="image"
+                  height={300}
+                  width={300}
+                />{" "}
+                {/* Display enlarged image on mobile screens */}
+              </div>
+              <div className="hidden md:flex">
+                <div className="w-1/2 mb-4 md:w-1/4 md:mb-0">
+                  <Image
+                    src={urlForImage(image[0]).url()}
+                    alt="image"
+                    height={100}
+                    width={100}
+                  />{" "}
+                  {/* Display small image */}
+                </div>
+                <div className="w-1/2 md:w-3/4 md:pl-8">
+                  <Image
+                    src={urlForImage(image[0]).url()}
+                    alt="image"
+                    height={300}
+                    width={300}
+                  />{" "}
+                  {/* Display enlarged image */}
                 </div>
               </div>
-              <div className="mt-4">
-                <button className="px-4 py-2 text-white bg-blue-500">
-                  Add to Cart
-                </button>
-              </div>
             </div>
-          </div>
-          <div className="flex">
-            <div className="w-full">
-              <h2 className="mt-8 text-2xl font-bold">Product Details</h2>
-              <hr className="my-4" />
-              <p>{details}</p>
+            <div className="w-full md:w-1/2">
+              <div>
+                <h1 className="text-2xl font-bold md:text-4xl">{title}</h1>
+                <h2 className="mt-4 text-2xl font-bold md:text-4xl">{price}</h2>
+                <div className="mt-4">
+                  <button className="px-4 py-2 text-white bg-blue-500">
+                    Add to Cart
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="w-full">
-            <h2 className="mt-8 text-2xl font-bold">Product Care</h2>
+            <h2 className="mt-8 text-2xl font-bold md:text-4xl">
+              Product Details
+            </h2>
+            <hr className="my-4" />
+            <p>{details}</p>
+          </div>
+          <div className="w-full">
+            <h2 className="mt-8 text-2xl font-bold md:text-4xl">
+              Product Care
+            </h2>
             <hr className="my-4" />
             <p>{care}</p>
           </div>
