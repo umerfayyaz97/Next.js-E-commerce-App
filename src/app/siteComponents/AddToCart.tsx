6ac6376764a5ee7ac2ee694946e3eb858a1e8290
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-07-28 09:12:22 +0500

    Redux implementation

--- src/app/siteComponents/AddToCart.tsx
+++ src/app/siteComponents/AddToCart.tsx
@@ -1,31 +1,4 @@
 "use client";
-// import { useSelector, useDispatch } from "react-redux";
-// import { RootState } from "@/store/store";
-// import { cartActions } from "@/store/slice/cartSlice";
-// import { Button } from "@/components/ui/button";
-
-// interface IProduct {
-//   title: string;
-//   image: typeof Image;
-//   price: number;
-//   _id: string;
-// }
-
-// const AddToCart = (item: IProduct[]) => {
-//   const dispatch = useDispatch();
-
-//   const addItem = () => {
-//     dispatch(cartActions.addToCart(item));
-//   };
-
-//   return (
-//     <div>
-//       <Button onClick={addItem}>Add to Cart</Button>
-//     </div>
-//   );
-// };
-
-// export default AddToCart;
 
 import React from "react";
 import { useDispatch } from "react-redux";
