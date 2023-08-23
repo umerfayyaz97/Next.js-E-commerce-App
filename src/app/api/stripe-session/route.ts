83d46b3b2f6fa9db586115e62022e9ec407e53c2
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-08-23 14:55:21 +0500

    Stripe

---
+++ src/app/api/stripe-session/route.ts
@@ -0,0 +1,95 @@
+// Import required dependencies and modules
+import { IProduct } from "@/views/utils/mock";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+// Get the Stripe secret key from environment variables
+const key = process.env.STRIPE_SECRET_KEY || "";
+
+// Initialize the Stripe instance
+const stripe = new Stripe(key, {
+  apiVersion: "2022-11-15",
+});
+
+// Handle GET request to retrieve the Stripe checkout session
+export async function GET(req: NextRequest) {
+  try {
+    // Retrieve the customer session ID from cookies
+    const customer_session = cookies().get("customer_session")?.value;
+
+    // Retrieve the Stripe checkout session using the customer session ID
+    const session = await stripe.checkout.sessions.retrieve(
+      customer_session as string
+    );
+
+    return NextResponse.json({ session });
+  } catch (error) {
+    // Return an error response in case of exceptions
+    return NextResponse.json({
+      message: "Some error occurred with customerSession",
+    });
+  }
+}
+
+// Handle POST request to create a new Stripe checkout session
+export async function POST(request: NextRequest) {
+  // Parse the JSON body of the request
+  const body = await request.json();
+  const setCookies = cookies();
+  const customer_session = cookies().get("customer_session");
+
+  try {
+    if (body.length > 0) {
+      // Create a new Stripe checkout session using the provided product data
+      const session = await stripe.checkout.sessions.create({
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1NiByGAwhifC9JLZYnMX5JKL" },
+          { shipping_rate: "shr_1NiBxIAwhifC9JLZ1aZIrVDa" },
+        ],
+        invoice_creation: {
+          enabled: true,
+        },
+        line_items: body.map((item: any) => {
+          return {
+            price_data: {
+              currency: "pkr",
+              product_data: {
+                name: item.title,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+              maximum: 10,
+            },
+          };
+        }),
+        phone_number_collection: {
+          enabled: true,
+        },
+        success_url: `${request.headers.get("origin")}/success`,
+        cancel_url: `${request.headers.get("origin")}/canceled`,
+      });
+
+      // Set the customer session cookie if not already present
+      if (!customer_session) {
+        setCookies.set("customer_session", session.id);
+      }
+
+      return NextResponse.json({ session });
+    } else {
+      return NextResponse.json({ message: "No Data Found" });
+    }
+  } catch (err: any) {
+    console.log(err);
+    // Return an error response in case of exceptions
+    return NextResponse.json(err.message);
+  }
+}
---
+++ src/app/siteComponents/StripeButton.tsx
@@ -0,0 +1,59 @@
+// Import necessary modules and components
+"use client";
+import getStripePromise from "@/lib/Checkout";
+// import { CartRootState } from "@/types/cart";
+import { useSelector } from "react-redux";
+// import { IProduct } from "./utils/mock";
+import toast from "react-hot-toast";
+
+// Component for the Stripe checkout button
+const StripeCheckOutButton = () => {
+  // Get cart items from Redux store
+  const cartItems: IProduct[] = useSelector(
+    (state: CartRootState) => state.cart.items
+  );
+
+  // Function to handle the checkout process
+  const handleCheckout = async () => {
+    try {
+      // Initialize Stripe using the getStipePromise function
+      const stripe = await getStripePromise();
+
+      // Send cart items to the server to create a Stripe session
+      const response = await fetch("/api/stripe-session/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        cache: "no-cache",
+        body: JSON.stringify(cartItems),
+      });
+
+      // Parse the response data
+      const data = await response.json();
+
+      // If session is created, redirect to Stripe checkout
+      if (data.session) {
+        stripe?.redirectToCheckout({ sessionId: data.session.id });
+      } else {
+        // Display an error toast if session creation fails
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log("stripe error", error);
+    }
+  };
+
+  // Render the Stripe checkout button
+  return (
+    <div className="py-5 text-center">
+      <button
+        className="bg-[#212121] font-[600] transition all hover:bg-white hover:text-black hover:border hover:border-black w-[155px] h-[42px] hover:border-solid px-2 py-1 rounded-md text-white"
+        onClick={handleCheckout}
+      >
+        Check out
+      </button>
+    </div>
+  );
+};
+
+// Export the StripeCheckOutButton component
+export default StripeCheckOutButton;
