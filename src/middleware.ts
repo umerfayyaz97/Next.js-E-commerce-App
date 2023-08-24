e2b52ac5f0ecdc93c0cd42cd4f2e7231c9927043
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2023-08-24 13:21:41 +0500

    Final Commits

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,34 @@ import { RedirectUrl } from "@clerk/nextjs/dist/types/server";
 import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
-
-  afterAuth(auth, req) {
-    if (!auth && req.url === "/Cart") {
+  afterAuth(auth, req, evt) {
+    // Redirect users who aren't authenticated to the sign-in page,
+    // except for public routes.
+    if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: req.url });
     }
+
+    // Lock the "success" page if the user is not authenticated.
+    // Redirect to sign-in, except for the "cancel" page.
+    if (!auth.userId && req.nextUrl.pathname === "/success") {
+      return redirectToSignIn({ returnBackUrl: "/Cart" });
+    }
   },
+
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/Male",
+    "/Female",
+    "/Kids",
+    "/All-Products",
+  ],
+
   // afterAuth(auth, req) {
-  // if (auth && req) {
-  //   return; // return redirectToSignIn({ returnBackUrl: req.url });
-  // }
+  //   if (!auth && req.url === "/Cart") {
+  //     return redirectToSignIn({ returnBackUrl: req.url });
+  //   }
   // },
 });
 
@@ -22,4 +39,4 @@ export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
 
-// "/Male", "/Female", "/Kids", "/All-Products"
+//
